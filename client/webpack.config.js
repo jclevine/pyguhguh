372458ae56ca7c8e62282ad006ea6a8f436b5ef5
Justin Levine <justin.levine@scotiabank.com>
2017-02-19 18:40:31 -0500

    [GUHGUH-1] So much work to understand webpack. Turns out the js file just needed to be below the html element it was manipulating. Oops.

---
+++ client/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    // new webpack.optimize.OccurrenceOrderPlugin(),
+    // new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}})
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};

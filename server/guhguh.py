edc514debd929c59a44d751d483968dc1db91f7d
Justin Levine <justin.levine@scotiabank.com>
2017-02-20 09:22:44 -0500

    [GUHGUH-1] Make flask's static directory empty for the sake of html paths.

--- server/guhguh.py
+++ server/guhguh.py
@@ -4,7 +4,7 @@ from views.gategory import gategory
 
 # TODO: jlevine - Maybe use Flask-Webpack? https://github.com/nickjj/flask-webpack
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 app.register_blueprint(gategory)
 
 if __name__ == '__main__':  # pragma: no cover

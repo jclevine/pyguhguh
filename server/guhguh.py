8aaa57fe61bdff44f6ad265785cc35088cfff091
Justin Levine <justin.levine@scotiabank.com>
2017-02-20 12:05:25 -0500

    [GUHGUH-1] Adding gzipping of javascript file. Will eventually serve all static files on nginx.

--- server/guhguh.py
+++ server/guhguh.py
@@ -3,6 +3,7 @@ from flask import Flask
 from views.gategory import gategory
 
 # TODO: jlevine - Maybe use Flask-Webpack? https://github.com/nickjj/flask-webpack
+# TODO: jlevine - Change Gategories into Catergoalies
 
 app = Flask(__name__, static_url_path='')
 app.register_blueprint(gategory)
--- server/views/gategory.py
+++ server/views/gategory.py
@@ -1,6 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory
 
+# TODO: jlevine - Go through named params and get rid of very obvious defaults.
 gategory = Blueprint(name='gategory', import_name=__name__, template_folder='templates')
+
+
+# TODO: jlevine - Should be using nginx or another web server for static files.
+@gategory.route('/js/<path:path>')
+def send_js(path):
+    response = send_from_directory('static/js', path)
+    response.headers['Content-Encoding'] = 'gzip'
+    return response
 
 
 @gategory.route('/gategory')

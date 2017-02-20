151401a6d4d67d984051b3a21e18cf2d6c1755c8
Justin Levine <justin.levine@scotiabank.com>
2017-02-20 08:48:25 -0500

    [GUHGUH-1] Remove unnecessary babel packages. Add hot-swap dev packages.

--- client/app/index.js
+++ client/app/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <h1>How about that, eh?!</h1>,
+  <h1>How about that, eh?! Eh!?</h1>,
   document.getElementById('gategories')
 );
